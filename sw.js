8c2fd4eb403218f671cf4b38f01e9cce881f4741
LitoLitoLito <https://github.com/LitoLitoLito/PYTHONcienciaDatos.git>
2025-01-21 00:51:37 -0300

    modifica imagenes

--- sw.js
+++ sw.js
@@ -5,7 +5,9 @@ self.addEventListener("install", event => {
                 "./index.html",
                 "./styles.css",
                 "./script.js",
-                "./manifest.json"
+                "./manifest.json",
+                "./icon-192.png", // Ícono pequeño
+                "./icon-512.png"  // Ícono grande
             ]);
         })
     );
@@ -14,6 +16,7 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
+            // Devuelve el recurso de la caché o lo obtiene de la red si no está almacenado.
             return response || fetch(event.request);
         })
     );
